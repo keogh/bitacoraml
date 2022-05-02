9afc6a11563cd67df2414cebc91cbfa1dd80e361
Isaac Zepeda <417939+keogh@users.noreply.github.com>
2022-05-02 08:51:16 -0600

    Add Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,12 +52,15 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-TJPNETL6H4", // Google Analytics / GA
+        ],
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
